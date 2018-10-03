87cc31023021ce281054e45b628e5944bf849c1a
Kat Kitay <katkitay@gmail.com>
2018-10-02 20:20:43 -0400

    release candidate 1.0

--- src/scripts/pretty-age.js
+++ src/scripts/pretty-age.js
@@ -1,26 +1,41 @@
 function PrettyAge(hours, long = false) {
   let text = '';
-  let days = Math.floor(hours / 24);
+  let hoursInTime = {
+    year: 24 * 7 * 4 * 12,
+    month: 24 * 7 * 4,
+    week: 24 * 7,
+    day: 24
+  }
+  let years = Math.floor(hours / hoursInTime.year);
+  hours = hours % hoursInTime.year;
+  let months = Math.floor(hours / hoursInTime.month);
+  hours = hours % hoursInTime.month;
+  let weeks = Math.floor(hours / hoursInTime.week);
+  hours = hours % hoursInTime.week;
+  let days = Math.floor(hours / hoursInTime.day);
+  hours = hours % hoursInTime.day;
+  
+  /*let days = Math.floor(hours / 24);
   let weeks = Math.floor(days / 7);
   let months = Math.floor(weeks / 4);
   let years = Math.floor(months / 12);
   let remainingMonths = months - years * 12;
   let remainingWeeks = weeks - remainingMonths * 4;
   let remainingDays = days - remainingWeeks * 7;
-  let remainingHours = hours - remainingMonths * 4 * 7 * 24 - remainingWeeks * 7 * 24 - remainingDays * 24;
-
+*/
+  //let remainingHours = hours - (years * 12 * 4 * 7 * 24) - (remainingMonths * 4 * 7 * 24) - (remainingWeeks * 7 * 24) - (remainingDays * 24);
   if(long === false) {
     if(years) text += years + 'y ';
-    if(remainingMonths) text += remainingMonths + 'm ';
-    if(remainingWeeks) text += remainingWeeks + 'w ';
-    if(remainingDays) text += remainingDays + 'd ';
-    if(!text) text = remainingHours + 'h';
+    if(months) text += months + 'm ';
+    if(weeks) text += weeks + 'w ';
+    if(days) text += days + 'd ';
+    if(!text) text = hours + 'h';
   } else {
     if(years) text += ' Year ' + years;
-    if(remainingMonths) text += ' Month ' + remainingMonths;
-    if(remainingWeeks) text += ' Week ' + remainingWeeks;
-    if(remainingDays) text += ' Day ' + remainingDays;
-    text += ' Hour ' + remainingHours;
+    if(months) text += ' Month ' + months;
+    if(weeks) text += ' Week ' + weeks;
+    if(days) text += ' Day ' + days;
+    text += ' Hour ' + hours;
   }
   return text.trim();
 }
